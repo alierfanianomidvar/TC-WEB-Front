63abadbcf75a06a28f6a3b629d7a4ad2e1e77b90
Kiarash <kiarash_23@yahoo.com>
2018-12-07 12:57:20 +0330

    fix: edit login api

--- js/main.js
+++ js/main.js
@@ -8,24 +8,24 @@ $("#myLogin").click(function () {
     $.ajax({
         type: "POST",
         data :JSON.stringify(person),
-        url: "http://172.20.10.6:5000/api/v1/auth/login",
+        url: "http://172.20.10.6:5000/api/v1/users/login",
         contentType: "application/json",
         dataType: "json",
         async: false,
         success: function(j){
             if (j.status == "invalid credentials!"){
                 window.alert("Invalid username and password");
-                window.location.replace("file:///D:/TC-WEB-Front/html/Login.html");
+                window.location.replace("http://172.20.10.6:5000/templates/html/Login.html");
             }else{
                 if (j.object.role=="admin") {
                     console.log("asasasasasa");
-                    window.location.replace("file:///D:/TC-WEB-Front/html/Adminpage.html");
+                    window.location.replace("http://172.20.10.6:5000/templates/html/Adminpage.html");
                 }
                 else {
                     console.log("bbbbbbbbbb");
-                    window.location.replace("file:///D:/TC-WEB-Front/html/User-profilepage.html");
+                    window.location.replace("http://172.20.10.6:5000/templates/html/User-profilepage.html");
                 }
-                 //user profile
+                //user profile
             }
         }
     })
