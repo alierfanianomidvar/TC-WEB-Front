eda64ee2a2337f01f07e04765599a367feaacd59
Kiarash <kiarash_23@yahoo.com>
2018-12-19 14:24:47 +0330

    feat: Add admin's user list js
    Admin can see list of users

---
+++ js/AdminUserList.js
@@ -0,0 +1,19 @@
+$.ajax({
+    url: "http://172.20.10.6:5000" + "/api/v1/users",
+    contentType: "application/json",
+    dataType: "json",
+    async: false,
+    success: function (j) {
+        var obj = j.object;
+        console.log(obj);
+        $(obj).each(function(i, item){
+            $("#myTitle" + (i+1)).append(item.name + " " + item.family);
+            $("#myName" + (i+1)).append(item.name);
+            $("#myFamily" + (i+1)).append(item.family);
+            $("#myAge" + (i+1)).append(item.age);
+            $("#myIdNum" + (i+1)).append(item.identificationId);
+            $("#myPhone" + (i+1)).append(item.mobile_num);
+            $("#myEmail" + (i+1)).append(item.email)
+        })
+    }
+});
