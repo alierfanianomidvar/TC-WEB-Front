e4b47d2ef4802eb6467b778d9949d2831eff338f
Kiarash <kiarash_23@yahoo.com>
2019-01-26 15:36:46 +0330

    fix: make car list dynamic

--- js/Admin.js
+++ js/Admin.js
@@ -116,6 +116,67 @@ $("#mySubmit").click(function () {
     })
 });
 
+var carId;
+
+var token = $.cookie("token");
+
+$.ajax({
+    url: ip + "/api/v1/cars",
+    contentType: "application/json",
+    dataType: "json",
+    async: false,
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader('Access-Token', token);
+    },
+    success: function (j) {
+        var obj = j.object;
+        $(obj).each(function(i, item){
+            /*$(".but" + (i+1)).attr("id","rent" + item.id);
+            $(".name" + (i+1)).append(item.name)
+            $(".company" + (i+1)).append(item.factory)
+            $(".cost" + (i+1)).append(item.price)
+            $(".kilometer" + (i+1)).append(item.kilometer)*/
+            // var gift = gifts[i];
+            // console.log(gift);
+            var box = document.createElement('div');
+            box.setAttribute('class', 'w3-half w3-margin-bottom col-sm-4');
+            var img = document.createElement('img');
+            img.src = "../img/6572131-tehran-wallpapers.jpg";
+            img.setAttribute('style' , 'width:100%')
+            var info = document.createElement('div');
+            info.setAttribute('class', 'w3-container w3-white');
+            var name = document.createElement('h3');
+            name.innerHTML = item.name;//kia
+            var companey = document.createElement('h4');
+            companey.innerHTML = item.factory;//kia
+            var cost = document.createElement('p');
+            cost.innerHTML = " Cost : " + item.price;//kia
+            var kilometer1 = document.createElement('p');
+            kilometer1.innerHTML = item.kilometer;//kia
+            var button = document.createElement('button');
+            button.setAttribute('class', 'w3-button w3-margin-bottom');
+            button.setAttribute('name','rent');//kia
+            button.setAttribute('id',item.id);//kia
+            button.setAttribute('onclick','confirmGiftInfo(this)');
+            button.innerHTML = "Profile";
+            info.appendChild(name);
+            info.appendChild(companey);
+            info.appendChild(cost);
+            info.appendChild(kilometer1);
+            info.appendChild(button);
+            box.appendChild(img);
+            box.appendChild(info)
+            document.getElementById('CarList').appendChild(box);
+        })
+    }
+});
+$("button[name=rent]").click(function () {
+    var myId = $(this).attr("id");
+    carId = $.cookie("carId",myId);
+    window.location.replace("../html/AdminCarProfile.html")
+});
+
+
 $("#homeButt").click(function () {
     window.location.replace("../html/Adminpage.html")
 });
