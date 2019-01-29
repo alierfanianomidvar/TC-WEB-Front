b253177e64003c7d06fa90cdb3731f9f824a4ff3
Ali <ali.erfanianomidvar@gmail.com>
2019-01-29 18:28:57 +0330

    fix : height of photo fixed.

--- js/UserProfile.js
+++ js/UserProfile.js
@@ -25,6 +25,7 @@ $.ajax({
             box.setAttribute('class', 'w3-half w3-margin-bottom col-sm-4');
             var img = document.createElement('img');
             img.src = ip + "/api/v1/images/" + item.image_url;
+            img.setAttribute('class', 'pic');
             img.setAttribute('style' , 'width:100%')
             var info = document.createElement('div');
             info.setAttribute('class', 'w3-container w3-white');
